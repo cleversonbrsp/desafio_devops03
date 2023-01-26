8128b47f68d8b2e73bade7638b9f9db5a7024db2
Cleverson <68443857+thefly72003@users.noreply.github.com>
2023-01-26 15:19:17 -0300

    Add files via upload

---
+++ local_file.tf
@@ -0,0 +1,4 @@
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+do_token     = "dop_v1_f34e1daced12e92526cb0c584dc38251d3b225cbacf1fedf402f61cd73d7e526"
+ssh_key_name = "Jornada"
+region       = "nyc1"
